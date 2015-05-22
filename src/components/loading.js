6f803be63ea98874b677b23bda93817ade9bb0b8
Rolf Erik Lekang <me@rolflekang.com>
2015-05-22 19:00:12 +0200

    Fix #6, add message to minimal loader

--- src/components/loading.js
+++ src/components/loading.js
@@ -1,12 +1,22 @@
 import React from 'react';
 
-export default class BuildDetails extends React.Component {
+export default class Loading extends React.Component {
   render() {
-    var iconClasses = 'loading fa fa-spinner fa-pulse fa-4x'
-    if (this.props.minimal === true) iconClasses += ' minimal';
+    var message = false;
+    var classes = 'text-center';
+    var iconClasses = 'loading fa fa-spinner fa-pulse';
+
+    if (this.props.minimal === true) {
+      iconClasses += ' minimal';
+      classes += ' alert alert-info'
+      message = 'We are fetching the latest builds for you';
+    } else {
+      iconClasses += ' fa-4x';
+    }
+
     return (
-      <div className="text-center">
-        <i className={iconClasses}></i>
+      <div className={classes}>
+        {message} <i className={iconClasses}></i>
       </div>
     );
   }
