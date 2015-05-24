e6170674bacf9cfe440c3c4203760ad6824ffddc
Rolf Erik Lekang <me@rolflekang.com>
2015-05-24 22:01:55 +0200

    Add navigation for staff

--- src/components/header.js
+++ src/components/header.js
@@ -9,10 +9,36 @@ export default class Header extends React.Component {
           <Link to="builds" className="brand">Frigg CI</Link>
           <div className="navigation">
             <Link to="builds" className="button">Builds</Link>
-            <a href="/auth/logout/" className="button">Logout</a>
+            <StaffButton href="/stats/" text="Stats" {...this.props.user} />
+            <StaffButton href="/admin/" text="Admin" {...this.props.user} />
+            <LogInOutButton {...this.props.user} />
           </div>
         </div>
       </div>
+    );
+  }
+}
+
+export class LogInOutButton extends React.Component {
+  render() {
+    if (this.props.is_anonymous === false) {
+      return (
+        <a href="/auth/logout/" className="button">Logout</a>
+      );
+    } else {
+      return (
+        <a href="/auth/login/" className="button">Login</a>
+      );
+    }
+  }
+}
+
+
+export class StaffButton extends React.Component {
+  render() {
+    if (this.props.is_staff === false) return false;
+    return (
+      <a href={this.props.href} className="button">{this.props.text}</a>
     );
   }
 }
