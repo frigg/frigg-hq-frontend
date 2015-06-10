b4edfc4f4b7043dded786790f4ff161a4cc153ed
Rolf Erik Lekang <me@rolflekang.com>
2015-06-10 11:11:04 +0200

    Add title to footer icon links

--- src/components/footer.js
+++ src/components/footer.js
@@ -7,10 +7,10 @@ export default class Footer extends React.Component {
       <div className="footer">
         <div className="container">
           <div className="icon-links">
-            <a href="https://frigg.io/docs/"><i className="fa fa-book"></i></a>
-            <a href="https://github.com/frigg"><i className="fa fa-github-square"></i></a>
-            <a href="https://frigg.io/feed.xml"><i className="fa fa-rss-square"></i></a>
-            <a href="https://twitter.com/frigg_io"><i className="fa fa-twitter-square"></i></a>
+            <a href="https://frigg.io/docs/" title="Documentation"><i className="fa fa-book"></i></a>
+            <a href="https://github.com/frigg" title="Github"><i className="fa fa-github-square"></i></a>
+            <a href="https://frigg.io/feed.xml" title="News Feed"><i className="fa fa-rss-square"></i></a>
+            <a href="https://twitter.com/frigg_io" title="Twitter"><i className="fa fa-twitter-square"></i></a>
           </div>
           {this.props.user.is_staff ? (<div className="text-center">
             <a href="https://frigg.io/terms">Terms of Service</a>
