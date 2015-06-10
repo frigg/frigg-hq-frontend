7a43eab3b78f4f872787b66c614184d22ca6f9a7
Rolf Erik Lekang <me@rolflekang.com>
2015-06-10 16:35:19 +0200

    Fix #15, Add offline error message
    Will show an error message when the application is offline and there
    is no data in the localstorage.

--- src/components/loading.js
+++ src/components/loading.js
@@ -1,14 +1,23 @@
 import React from 'react';
 
+import strings from '../strings';
+
 export default class Loading extends React.Component {
   render() {
     var message = false;
-    var classes = 'text-center';
-    var iconClasses = 'loading fa fa-spinner fa-pulse fa-4x';
+    var classes = 'text-center loading';
+    var iconClasses = 'fa fa-spinner fa-pulse fa-4x';
+
+    if (!navigator.onLine) {
+      message = (
+        <h2>{strings.OFFLINE_NO_DATA}</h2>
+      );
+    }
 
     return (
       <div className={classes}>
-        {message} <i className={iconClasses}></i>
+        {message}
+        <p><i className={iconClasses}></i></p>
       </div>
     );
   }
--- src/strings.js
+++ src/strings.js
@@ -2,5 +2,6 @@
 export default {
   LOADING: 'Loading',
   FOUR_O_FOUR: 'We could not find what you were looking for.',
-  BUILD_ERRORED: 'The build encountered an error.'
+  BUILD_ERRORED: 'The build encountered an error.',
+  OFFLINE_NO_DATA: 'You are offline and we do not have any local data, please go online to fetch data.'
 };
